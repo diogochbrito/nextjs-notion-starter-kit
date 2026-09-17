ea25a816a7f4ba1bd7acd7bad69c5407d5ed0244
Diogo BRITO <62956779+diogochbrito@users.noreply.github.com>
2026-09-18 00:29:17 +0100

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -1,6 +1,6 @@
 import { siteConfig } from './lib/site-config'
 
-export const config = {
+export default siteConfig({
   // ID de 32 carateres limpo (sem hífens e sem o URL todo)
   rootNotionPageId: '3d98d0ec315c80d5b1aaf74bd5f84680',
 
@@ -21,6 +21,6 @@ export const config = {
   pageUrlOverrides: null,
 
   // Funcionalidades
-  isPreviewImageSupportEnabled: false, // Mantenha false para evitar erros de renderização de imagem no build
+  isPreviewImageSupportEnabled: false, // Mantenha false para evitar erros no build
   isSearchEnabled: false
-}
+})
