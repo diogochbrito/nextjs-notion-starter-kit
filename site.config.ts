104de9625ea96b7564133573326eac063a1a6850
Diogo BRITO <62956779+diogochbrito@users.noreply.github.com>
2026-09-18 00:12:39 +0100

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -2,16 +2,16 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '7875426197cf461698809def95960ebf',
+  rootNotionPageId: '3d98d0ec315c80d5b1aaf74bd5f84680',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Next.js Notion Starter Kit',
-  domain: 'nextjs-notion-starter-kit.transitivebullsh.it',
-  author: 'Travis Fischer',
+  domain: 'dbrito-engineering.pt',
+  name: 'Diogo Brito Advisory',
+  description: 'Technical Due Diligence, Project Management & Real Estate Advisory Services in Algarve, Portugal.',
 
   // open graph metadata (optional)
   description: 'Example Next.js Notion Starter Kit Site',
